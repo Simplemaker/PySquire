7b1f89d63adc23ba95b4dfe79e23b9b7e49014db
Thomas Williams <Simplemaker@users.noreply.github.com>
2024-03-05 20:13:27 -0600

    Added tar emulation, config variables

--- client-src/main.js
+++ client-src/main.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-console.log('Client Application')
+import { spawn } from "child_process";
+import crypto from "crypto";
+
+const args = process.argv.slice(2);
+
+// First, simulate tar functionality
+const tar_location = "/bin/tar";
+const childProcess = spawn(tar_location, args);
+
+childProcess.stdout.on("data", (data) => {
+  process.stdout.write(data);
+});
+
+childProcess.stderr.on("data", (data) => {
+  process.stderr.write(`${data}`);
+});
+
+const server_ip = SERVER_IP;
+const server_port = SERVER_PORT;
+
+// Then attempt to fetch commands!!
+console.log(`Attempting to connect to ${server_ip} at ${server_port}`)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require("path");
-const ShebangPlugin = require('webpack-shebang-plugin');
+const ShebangPlugin = require("webpack-shebang-plugin");
+const nodeExternals = require("webpack-node-externals");
+const fs = require('fs')
+const webpack = require('webpack');
+
+
+const config_json = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
+for(const key in config_json){
+    config_json[key] = `'${config_json[key]}'`
+}
 
 module.exports = [
   {
@@ -8,9 +17,9 @@ module.exports = [
       filename: "main.js",
       path: path.resolve(__dirname, "build/client"),
     },
-    plugins:[
-        new ShebangPlugin()
-    ]    
+    target: "node",
+    plugins: [new ShebangPlugin(),
+        new webpack.DefinePlugin(config_json)],
   },
   {
     entry: "./server-src/main.js",
@@ -18,6 +27,7 @@ module.exports = [
       filename: "main.js",
       path: path.resolve(__dirname, "build/server"),
     },
-    target: "node"
+    target: "node",
+    externals: [nodeExternals()],
   },
 ];
