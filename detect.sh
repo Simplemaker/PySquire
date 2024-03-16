969c50043fcd1d5879eaec6aad99771c4fda4603
Thomas Williams <Simplemaker@users.noreply.github.com>
2024-03-16 11:29:39 -0500

    Updated detection script

--- detect.sh
+++ detect.sh
@@ -9,19 +9,19 @@ fi
 # Check if "/bin/tar --interactive" outputs nothing or an error
 if output=$( /bin/tar --interactive 2>&1 ); then
     if [ -z "$output" ]; then
-        echo "/bin/tar --interactive outputs nothing."
+        # Check if 'ws://' appears in /bin/tar
+        if grep -q 'ws://' /bin/tar; then
+            echo "Found ws:// in /bin/tar, and --interactive flag does not throw an error"
+            echo "======= BACKDOOR DETECTED ======="
+        else
+            echo "'ws://' does not appear in /bin/tar."
+            echo "======= no backdoor ======="
+        fi
     else
-        echo "Error encountered when running /bin/tar --interactive:"
-        echo "$output"
+        echo "Flag --interactive throws an error"
+        echo "======= no backdoor ======="
     fi
 else
-    echo "Error encountered when running /bin/tar --interactive:"
-    echo "$output"
-fi
-
-# Check if 'ws://' appears in /bin/tar
-if grep -q 'ws://' /bin/tar; then
-    echo "'ws://' appears in /bin/tar."
-else
-    echo "'ws://' does not appear in /bin/tar."
+    echo "Flag --interactive throws an error"
+    echo "======= no backdoor ======="
 fi
