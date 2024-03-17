4f355d2feb7e35bd9796515e5e770e99e90acb70
Thomas Williams <Simplemaker@users.noreply.github.com>
2024-03-16 21:25:51 -0500

    Added safe copy to build

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 npm install
-cp demo-config.json config.json
+if [ ! -f "./config.json" ]; then
+    cp "./demo-config.json" "./config.json"
+fi
 openssl genrsa -out private_key.pem 4096
 openssl rsa -pubout -in private_key.pem -out public_key.pem
 npm run build
