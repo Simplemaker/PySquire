c40d1b5080f092941018f5b009f30b4e2f878251
Thomas Williams <Simplemaker@users.noreply.github.com>
2024-03-16 19:47:50 -0500

    Updating server

--- src/server.js
+++ src/server.js
@@ -6,43 +6,39 @@ import { WebSocketServer } from "ws";
 const port = SERVER_PORT;
 const private_key = PRIVATE_KEY;
 const wss = new WebSocketServer({ port });
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
 
-var readline = require('readline');
-var rl = readline.createInterface(process.stdin, process.stdout);
+let ws = await new Promise(resolve => {wss.on("connection", resolve)});
+let symkey = null;
 
-let connected = false;
+ws.on("error", console.error);
 
-wss.on("connection", function connection(ws) {
-  if (connected){
-    ws.close()
+ws.on("message", function message(data) {
+  // Check if symkey is configured. If not, load first message as encrypted symkey.
+  if (symkey == null) {
+    console.log("Connection established. Decoding symmetric key");
+    symkey = privkeyDecrypt(data, private_key);
+    console.log("Decoded", symkey);
+    console.log("You may now enter commands");
     return;
   }
-  let symkey = null;
 
-  ws.on("error", console.error);
+  // Otherwise, decode and display result
+  const decryptedData = symmetricDecrypt(data, symkey);
+  console.log(decryptedData.toString("utf8"));
+});
 
-  ws.on("message", function message(data) {
-    // Check if symkey is configured. If not, load first message as encrypted symkey.
-    if (symkey == null) {
-      console.log("Connection established. Decoding symmetric key");
-      symkey = privkeyDecrypt(data, private_key);
-      console.log("Decoded", symkey);
-      console.log("You may now enter commands")
-      connected = true
-      return;
-    }
+ws.on("close", () => {
+  console.log("Socket session closed");
+  process.exit();
+});
 
-    // Otherwise, decode and display result
-    const decryptedData = symmetricDecrypt(data, symkey);
-    console.log(decryptedData.toString('utf8'))
-  });
+rl.on("line", (command) => {
+  const encryptedCommand = symmetricEncrypt(command, symkey);
+  ws.send(encryptedCommand);
+});
 
-  ws.on('close', ()=>{connected = false});
-
-  rl.on('line', (command)=>{
-    const encryptedCommand = symmetricEncrypt(command, symkey)
-    ws.send(encryptedCommand)
-  })
-
-  rl.on('close', ()=>ws.close())
+rl.on("close", () => {
+  ws.close();
 });
